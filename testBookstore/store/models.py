50cd4342fa4e6f6aaff7682118a24a52bd68f218
Ruiwen Hou <abc@example.com>
2020-01-30 21:15:38 -0600

    This is the model update 2.0

--- testBookstore/store/models.py
+++ testBookstore/store/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+from django.db import models
+import uuid
+from django.contrib.postgres.fields import ArrayField
+
+
+#Here is the model I created for books.
+class Book(models.Model):
+    def __str__(self):
+        return self
+    
+    isbn_13 = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    description = models.CharField(max_length=700)
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=200, null=True)
+    series = models.CharField(max_length=100,null=True)
+    volume = models.CharField(max_length=100,null=True)
+    ready_for_sale = models.BooleanField()
+    # should we have multiple places for publisher and author since we might have more than one publisher and author.
+    publisher = ArrayField(models.CharField(max_length=100))
+    author = ArrayField(models.CharField(max_length=300))
+
+
+   
+
+    
+
+
+#Here is the modele for site Book Data
+#class Site_book_data():
+ #   def __str__(self):
+  #      return self
+   # book_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
