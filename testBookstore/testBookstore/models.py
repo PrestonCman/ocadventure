47d429ae5fe15417807f9ab3cac5364d6743b475
Ruiwen Hou <abc@example.com>
2020-01-30 20:34:39 -0600

    Added ready for sale

--- testBookstore/testBookstore/models.py
+++ testBookstore/testBookstore/models.py
@@ -8,34 +8,28 @@ class Book():
     def __str__(self):
         return self
     
-    
+    isbn_13 = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     description = models.CharField(max_length=700)
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=200, null=True)
     series = models.CharField(max_length=100,null=True)
     volume = models.CharField(max_length=100,null=True)
+    ready_for_sale = models.BooleanField()
     # should we have multiple places for publisher and author since we might have more than one publisher and author.
     publisher = ArrayField(models.CharField(max_length=100))
     author = ArrayField(models.CharField(max_length=300))
-    book_format = ArrayField(models.CharField(max_length=100))
 
-    #Here are the mdoels for different formats
-    isbn_13_print = models.CharField(max_length=100)
-    isbn_13_kindle = models.CharField(max_length=100)
-    isbn_13_audio = models.CharField(max_length=100)
-    isbn_13_digital = models.CharField(max_length=100)
-    price_print = models.CharField(max_length=100)
-    price_kindle = models.CharField(max_length=100)
-    price_audio = models.CharField(max_length=100)
-    price_digital = models.CharField(max_length=100)
+
+   
 
     
 
 
 #Here is the modele for site Book Data
-class Site_book_data():
-    def __str__(self):
-        return self
-    book_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#class Site_book_data():
+ #   def __str__(self):
+  #      return self
+   # book_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
