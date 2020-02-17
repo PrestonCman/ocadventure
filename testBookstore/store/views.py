2732302b437339fb52b4ab59a6ff437ef71b5b56
Ruiwen Hou <abc@example.com>
2020-02-17 12:11:04 -0600

    This is a book detail page done, But pagination need to be done branch.

--- testBookstore/store/views.py
+++ testBookstore/store/views.py
@@ -15,7 +15,8 @@ def bookDetail(request, book_id):
 class search(ListView):
     model = Book
     template_name = 'store/search.html'
-    paginate_by = 5
+    paginate_by = 2
+
     #q is the name given to the user input, object list is the resulting query set after checking 
     #all the books if ISBN, primary author, other authors, or title contains the user query.
     def get_queryset(self):
