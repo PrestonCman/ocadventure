f0eae1c681050f171181f554350071b9f245634f
StEmerson <Stephen.emerson@eagles.oc.edu>
2020-02-17 12:05:03 -0600

    Revert "added functionality to allow for pagination without throwing an error"
    This reverts commit 1298a4a03f15186f9a78406e80deb75964b98770.

--- testBookstore/store/views.py
+++ testBookstore/store/views.py
@@ -15,14 +15,15 @@ def bookDetail(request, book_id):
 class search(ListView):
     model = Book
     template_name = 'store/search.html'
-    paginate_by = 5
+
     #q is the name given to the user input, object list is the resulting query set after checking 
     #all the books if ISBN, primary author, other authors, or title contains the user query.
     def get_queryset(self):
         query = self.request.GET.get('q')
-        queryset = Book.objects.filter(Q(title__icontains=query) | Q(primary_author__icontains=query) |
+        object_list = Book.objects.filter(Q(title__icontains=query) | Q(primary_author__icontains=query) |
                                           Q(other_authors__icontains=query) | Q(isbn_13__icontains=query))
-        return queryset
+        print(object_list)      #remove this later when html list display is added.  
+        return object_list
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data()
