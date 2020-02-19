ac99b08461c0516a8f642076333e9bcb38428cc3
Jacob Creech <jacob.creech@eagles.oc.edu>
2020-02-18 18:20:31 -0600

    Actually cleaned description!

--- testBookstore/store/models.py
+++ testBookstore/store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from sanitizer.models import SanitizedCharField
 from pygments.lexers import get_all_lexers
+import lxml.html.clean as html_cleaner, lxml.html as html
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 
@@ -9,7 +9,7 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 class Book(models.Model):
     isbn_13 = models.CharField(max_length=100)
     price = models.CharField(max_length=100)
-    description = SanitizedCharField(max_length=10000, strip=True)
+    description = models.CharField(max_length=10000)
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200, null=True, blank = True)
     series = models.CharField(max_length=100,null=True, blank = True)
@@ -22,6 +22,12 @@ class Book(models.Model):
     class Meta:
         ordering = ['title']
     
+    def clean_description(self):
+        doc = html.fromstring(self.description)
+        cleaner = html_cleaner.Cleaner(style=True)
+        doc = cleaner.clean_html(doc)
+        return doc.text_content()
+        
     def __str__(self):
        return self.title
    
