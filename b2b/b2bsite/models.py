740a52b1f09379365e1f1387c2e5254c90d50b78
Ruiwen Hou <abc@example.com>
2020-04-06 15:57:11 -0500

    This is the usergroups and model done.

--- b2b/b2bsite/models.py
+++ b2b/b2bsite/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200) 
+    company = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    num_queries = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
