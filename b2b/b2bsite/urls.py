1629c30e53a64f327c5d4fca35c1de692bc25365
StEmerson <Stephen.emerson@eagles.oc.edu>
2020-04-19 00:38:14 -0500

    Created search results page

--- b2b/b2bsite/urls.py
+++ b2b/b2bsite/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('search/', views.search, name='search')
+    #path('login/', views.login, name='login'),
+    path('search/', views.search, name='search'),
+    path('results/', views.do_search, name='results'),
 ]
